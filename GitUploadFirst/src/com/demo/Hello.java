762e783bc39e4357843242a4a16cbea17f03a309
Sameer <sameerghogare000@gmail.com>
2023-03-16 18:10:15 +0530

    added a arraylist to code

--- GitUploadFirst/src/com/demo/Hello.java
+++ GitUploadFirst/src/com/demo/Hello.java
@@ -1,7 +1,17 @@
 package com.demo;
+
+import java.util.ArrayList;
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+	/*add later hence commited again*/
+	ArrayList<Integer> intt= new ArrayList<Integer>();
+	intt.add(3);
+	intt.add(3234);
+	intt.add(321);
+	intt.add(312);
+	intt.add(321);
 	}
 
 }
